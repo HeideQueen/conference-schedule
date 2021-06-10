5cb6d7c65833ff78f50c4128a1098c9e4b9971cd
HeideQueen <lucyfabz@gmail.com>
2021-06-10 01:46:20 -0500

    footer moved into component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="bg-pink-600 text-gray-50 flex justify-center items-center h-14 font-light">
+      <p>RabbitCon is brought to you by the Usada Kensetsu Corp. 2021</p>
+    </footer>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,13 @@
 import React from "react"
 import Navbar from "./Navbar"
+import Footer from "./Footer"
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
-      <div>{children}</div>
-      <footer className="bg-pink-600 text-gray-50 flex justify-center items-center h-14">
-        <p>RabbitCon is brought to you by the Usada Kensetsu Corp. 2021</p>
-      </footer>
+      <div className="container">{children}</div>
+      <Footer />
     </div>
   )
 }
